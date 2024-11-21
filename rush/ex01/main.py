7a36f25b44c2fafbf0f34ab1957c34778e74368c
kjinnuko <67011141@kmitl.ac.th>
2024-11-21 16:26:33 +0000

    Complete

--- rush/ex01/main.py
+++ rush/ex01/main.py
@@ -2,17 +2,16 @@ import sys
 from checkmate import checkmate
 
 def main():
-    # ตรวจสอบว่าได้รับอาร์กิวเมนต์ไฟล์หรือไม่
+    
     if len(sys.argv) < 2:
         print("Error")
         return
 
-    # อ่านไฟล์ที่ได้รับมา
+
     for filename in sys.argv[1:]:
         try:
             with open(filename, 'r') as file:
-                board = file.read().strip()  # อ่านเนื้อหาในไฟล์
-                # ตรวจสอบกระดานหมากรุก
+                board = file.read().strip()  
                 checkmate(board)
         except FileNotFoundError:
             print(f"Error: ไม่พบไฟล์ {filename}")
