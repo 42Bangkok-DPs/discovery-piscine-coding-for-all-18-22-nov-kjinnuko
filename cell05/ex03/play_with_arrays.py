d17d2f8c2d3a5c803309c7cddd5d20ac333c9b6d
kjinnuko <67011141@kmitl.ac.th>
2024-11-21 16:56:27 +0000

    Complete

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def main():
+    arr = [2, 8, 9, 48, 8, 22, -12, 2]
+    print(arr)
+    unique_values = set(arr)
+    print(unique_values)
+
+if __name__ == "__main__":
+    main()
